916993042b8f15f197f3358287f05e455fa136b1
Andrew Schutt <andrew@interactiveshift.com>
2017-03-27 14:11:06 -0500

    turn on twitter sending

--- web/controllers/submission_controller.ex
+++ web/controllers/submission_controller.ex
@@ -80,7 +80,7 @@ defmodule WebgeeksRaffle.SubmissionController do
       DoubleSubmission.changeset(winning_submission, %{winner: true})
       |> Repo.update
 
-      # ExTwitter.update("Congrats @#{winning_submission.twitter_handle}! You are a raffle winner! #dsmwebgeekfest")
+      ExTwitter.update("Congrats @#{winning_submission.twitter_handle}! You are a raffle winner! #dsmwebgeekfest")
 
       conn
       |> put_flash(:info, "Winner was: #{winning_submission.first_name} #{winning_submission.last_name} - #{winning_submission.twitter_handle}")
